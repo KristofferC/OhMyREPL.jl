7849dc55b7f0cd1a08dba74f26a1ffaa2f2b7340
Tony Kelman <tony@kelman.net>
2017-05-26 20:29:37 -0700

    set FORCE_COLOR in runtests.jl using withenv
    this restores whatever the previous value (or unset) was when finished

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,6 @@
-ENV["FORCE_COLOR"] = true
-
-include("test_custompass.jl")
-include("test_bracketmatch.jl")
-include("test_highlighter.jl")
-include("test_rainbowbrackets.jl")
+withenv("FORCE_COLOR" => true) do
+    include("test_custompass.jl")
+    include("test_bracketmatch.jl")
+    include("test_highlighter.jl")
+    include("test_rainbowbrackets.jl")
+end
