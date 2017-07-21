b83c83cbb832f52852d11ced4cf1f53bf3169b4c
Kristoffer Carlsson <kristoffer.carlsson@chalmers.se>
2017-07-21 21:09:09 +0200

    revert some stuff (#90)
    * Revert "add noop for buffer of IOBuffer (#88)"
    
    This reverts commit 22d54acff6604f6098f59730767b8857c05e8cc2.
    
    * Revert "only run certain test on linux"
    
    This reverts commit a6d98ef5be60b3479a0f6b01c6b512d6365b5aac.
    
    * Revert "Fix a number of instances of flickering (#86)"
    
    This reverts commit 52aa257967531612213a229a0958e033a017cdf0.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,4 @@
 withenv("FORCE_COLOR" => true) do
-    if is_linux()
-        include("flicker.jl")
-    end
     include("test_custompass.jl")
     include("test_bracketmatch.jl")
     include("test_highlighter.jl")
