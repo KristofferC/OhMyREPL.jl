af4e36866ecc938596726dca7fc467f6a3f6969b
Tony Kelman <tony@kelman.net>
2017-08-05 11:56:20 -0400

    Show any unexpected stderr output during __init__

--- src/OhMyREPL.jl
+++ src/OhMyREPL.jl
@@ -121,13 +121,20 @@ function __init__()
         end
     end
 
-    mktemp() do _, f
+    mktemp() do file, io
         old_stderr = STDERR
-        redirect_stderr(f)
+        redirect_stderr(io)
         Base.LineEdit.refresh_line(s) = (Base.LineEdit.refresh_multi_line(s); OhMyREPL.Prompt.rewrite_with_ANSI(s))
         include(joinpath(@__DIR__, "output_prompt_overwrite.jl"))
         include(joinpath(@__DIR__, "MarkdownHighlighter.jl"))
         redirect_stderr(old_stderr)
+        close(io)
+        for line in eachline(file)
+            if !ismatch(Regex("^WARNING: Method definition [refresh_line|display|term]" *
+                    ".* overwritten in module .* at $(@__DIR__).*\$"), line)
+                println(line)
+            end
+        end
     end
 end
 
