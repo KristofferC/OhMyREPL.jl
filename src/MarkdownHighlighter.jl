76f91b91d1cf2a6f5b6f999bf943d29610f5a467
Kristoffer Carlsson <kcarlsson89@gmail.com>
2018-02-07 13:36:10 +0100

    change julia prompt in doctests to red

--- src/MarkdownHighlighter.jl
+++ src/MarkdownHighlighter.jl
@@ -39,7 +39,7 @@ function Base.Markdown.term(io::IO, md::Base.Markdown.Code, columns)
             SYNTAX_HIGHLIGHTER_SETTINGS(crayons, tokens, 0)
             buff = IOBuffer()
             if lang == "jldoctest" || lang == "julia-repl"
-                print(buff, Crayon(foreground = :yellow, bold = true), "julia> ", Crayon(reset = true))
+                print(buff, Crayon(foreground = :red, bold = true), "julia> ", Crayon(reset = true))
             end
             for (token, crayon) in zip(tokens, crayons)
                 print(buff, crayon)
