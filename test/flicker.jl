d7e509645c83d1562338ea36b6d42313cb930a3e
Kristoffer Carlsson <kristoffer.carlsson@chalmers.se>
2019-02-05 15:35:17 +0100

    fixup tests (#149)
    * fixup tests

--- test/flicker.jl
+++ test/flicker.jl
@@ -1,7 +1,6 @@
 using OhMyREPL
 import REPL
-
-include(Pkg.dir("VT100","test","TerminalRegressionTests.jl"))
+import TerminalRegressionTests
 
 TerminalRegressionTests.automated_test(
     joinpath(@__DIR__,"flicker/simple.multiout"),
@@ -13,7 +12,7 @@ TerminalRegressionTests.automated_test(
     [c for c in "function foo(a = b())\nend\n\x4"],
     aggressive_yield=true) do emuterm
     repl = REPL.LineEditREPL(emuterm, false)
-    repl.specialdisplay = val->display(REPL.REPLDisplay(repl), val)
+    repl.specialdisplay = REPL.REPLDisplay(repl)
     repl.interface = REPL.setup_interface(repl)
     OhMyREPL.Prompt.insert_keybindings(repl)
     REPL.run_repl(repl)
