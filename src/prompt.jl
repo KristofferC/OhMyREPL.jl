aa383692a54d08034c8ac1fba96e48b8ff3bee3b
Kristoffer Carlsson <kricarl@student.chalmers.se>
2020-04-07 10:34:43 +0200

    allow using functions for prompt modifications (#198)
    * allow using functions for prompt modifications
    
    * allow using functions for prompt modifications

--- src/prompt.jl
+++ src/prompt.jl
@@ -7,17 +7,18 @@ function update_interface(interface)
     main_mode.prompt = INPUT_PROMPT
 end
 
-function input_prompt!(s::String, col = :green)
+function input_prompt!(s::Union{String, Function}, col = :green)
     global INPUT_PROMPT_PREFIX = get(Base.text_colors, Symbol(col), "\e[2m")
-    global INPUT_PROMPT = isa(s, String) ? s * " " : s
+    global INPUT_PROMPT = isa(s, String) ? s * " " : () -> string(s(), " ")
     isdefined(Base, :active_repl) && isdefined(Base.active_repl, :interface) && update_interface(Base.active_repl.interface)
     return
 end
 
 global OUTPUT_PROMPT = ""
+global OUTPUT_PROMPT_PREFIX = ""
 
-function output_prompt!(s::String, col = :red)
-    global OUTPUT_PROMPT
-    OUTPUT_PROMPT = string("\e[1m", get(Base.text_colors, Symbol(col), Base.text_colors[:red]), s, "\e[0m", " ")
+function output_prompt!(s::Union{String, Function}, col = :red)
+    global OUTPUT_PROMPT_PREFIX = string("\e[1m", get(Base.text_colors, Symbol(col), Base.text_colors[:red]))
+    global OUTPUT_PROMPT = s
     return
 end
