9b411e7bb2fa5fae33b35e4ba065cbf7fd89a4c3
Jakob Nybo Nissen <jakobnybonissen@gmail.com>
2020-09-01 13:02:10 +0200

    Remove space in output_prompt_overwrite (#212)

--- src/prompt.jl
+++ src/prompt.jl
@@ -9,7 +9,7 @@ end
 
 function input_prompt!(s::Union{String, Function}, col = :green)
     global INPUT_PROMPT_PREFIX = get(Base.text_colors, Symbol(col), "\e[2m")
-    global INPUT_PROMPT = isa(s, String) ? s * " " : () -> string(s(), " ")
+    global INPUT_PROMPT = s
     isdefined(Base, :active_repl) && isdefined(Base.active_repl, :interface) && update_interface(Base.active_repl.interface)
     return
 end
