3d6981f2ef4cdf40357b4959151d2707adff7b64
Roger-Luo <rogerluo.rl18@gmail.com>
2021-06-23 02:44:44 -0400

    fix wrong dispatch

--- src/output_prompt_overwrite.jl
+++ src/output_prompt_overwrite.jl
@@ -6,21 +6,28 @@ function REPL.display(d::REPL.REPLDisplay, mime::MIME"text/plain", x)
     write(io, output_prompt, "\e[0m")
     Base.have_color && write(io, REPL.answer_color(d.repl))
     limited_io = IOContext(io, :limit => true)
-    mt = methods(Base.show, (typeof(io), MIME"text/plain", typeof(x)))
 
-    if !any(mt.ms) do method
-            method.sig == Tuple{typeof(Base.show), IO, MIME"text/plain", Any}
-        end
-        show(limited_io, mime, x)
+    if fallback_to_default_show(io, x)
+        pprint(limited_io, x)
     else
-        mt = methods(Base.show, (typeof(io), typeof(x)))
-        if !any(mt.ms) do method
-                method.sig == Tuple{typeof(Base.show), IO, MIME"text/plain", Any}
-            end
-            show(limited_io, mime, x)
-        else
-            pprint(limited_io, x)
-        end
+        show(limited_io, mime, x)
     end
     println(io)
+end
+
+function fallback_to_default_show(io::IO, x)
+    # NOTE: Base.show(::IO, ::MIME"text/plain", ::Any) forwards to
+    # Base.show(::IO, ::Any)
+
+    # check if we are gonna call Base.show(::IO, ::MIME"text/plain", ::Any)
+    mt = methods(Base.show, (typeof(io), MIME"text/plain", typeof(x)))
+    length(mt.ms) == 1 && any(mt.ms) do method
+        method.sig == Tuple{typeof(Base.show), IO, MIME"text/plain", Any}
+    end || return false
+
+    # check if we are gonna call Base.show(::IO, ::Any)
+    mt = methods(Base.show, (typeof(io), typeof(x)))
+    length(mt.ms) == 1 && return any(mt.ms) do method
+        method.sig == Tuple{typeof(Base.show), IO, Any}
+    end
 end
