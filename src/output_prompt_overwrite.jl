9d45c2d72baf06b0270049c104945739a7d738f5
Roger-Luo <rogerluo.rl18@gmail.com>
2021-06-22 20:54:12 -0400

    add garish print

--- src/output_prompt_overwrite.jl
+++ src/output_prompt_overwrite.jl
@@ -5,6 +5,11 @@ function REPL.display(d::REPL.REPLDisplay, mime::MIME"text/plain", x)
     write(io, OUTPUT_PROMPT_PREFIX)
     write(io, output_prompt, "\e[0m")
     Base.have_color && write(io, REPL.answer_color(d.repl))
-    show(IOContext(io, :limit => true), mime, x)
+    limited_io = IOContext(io, :limit => true)
+    if hasmethod(Base.show, Tuple{typeof(limited_io), MIME"text/plain", typeof(x)})
+        show(limited_io, mime, x)
+    else
+        pprint(limited_io, mime, x)
+    end
     println(io)
 end
