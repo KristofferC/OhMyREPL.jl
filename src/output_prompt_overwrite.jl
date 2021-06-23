4c937fad8cf0eca223949e07ee4fe6ee6f349c64
Roger-Luo <rogerluo.rl18@gmail.com>
2021-06-23 01:53:59 -0400

    change it back

--- src/output_prompt_overwrite.jl
+++ src/output_prompt_overwrite.jl
@@ -5,11 +5,6 @@ function REPL.display(d::REPL.REPLDisplay, mime::MIME"text/plain", x)
     write(io, OUTPUT_PROMPT_PREFIX)
     write(io, output_prompt, "\e[0m")
     Base.have_color && write(io, REPL.answer_color(d.repl))
-    limited_io = IOContext(io, :limit => true)
-    if hasmethod(Base.show, Tuple{typeof(limited_io), MIME"text/plain", typeof(x)})
-        show(limited_io, mime, x)
-    else
-        pprint(limited_io, mime, x)
-    end
+    show(IOContext(io, :limit => true), mime, x)
     println(io)
 end
