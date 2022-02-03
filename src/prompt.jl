ca5b4e8a58ee7009b0e10217bdd24c4ee786573f
Erik Loualiche <eloualic@umn.edu>
2022-02-03 11:15:42 -0600

    Update prompt.jl
    Allow for integer to call Base.text_colors to change the colors in the prompt.

--- src/prompt.jl
+++ src/prompt.jl
@@ -8,7 +8,7 @@ function update_interface(interface)
 end
 
 function input_prompt!(s::Union{String, Function}, col = :green)
-    global INPUT_PROMPT_PREFIX = get(Base.text_colors, Symbol(col), "\e[2m")
+    global INPUT_PROMPT_PREFIX = get(Base.text_colors, col isa Int64 ? col : Symbol(col), "\e[2m")
     global INPUT_PROMPT = s
     isdefined(Base, :active_repl) && isdefined(Base.active_repl, :interface) && update_interface(Base.active_repl.interface)
     return
